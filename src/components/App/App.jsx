13787920572218e5352472aa983c682bc180c0cd
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-14 18:18:26 +0300

    feat(router): implement routing with react-router for main pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,32 @@
-import './App.module.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../../pages/HomePage/HomePage';
+import NewsPage from '../../pages/NewsPage/NewsPage';
+import NoticesPage from '../../pages/NoticesPage/NoticesPage';
+import OurFriendsPage from '../../pages/OurFriendsPage/OurFriendsPage';
+import RegistrationPage from '../../pages/RegistrationPage/RegistrationPage';
+import LoginPage from '../../pages/LoginPage/LoginPage';
+import ProfilePage from '../../pages/ProfilePage/ProfilePage';
+import AddPetPage from '../../pages/AddPetPage/AddPetPage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+import Layout from '../../components/Layout';
+// import './App.module.css';
 
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} redirectTo="/home" />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/notices" element={<NoticesPage />} />
+        <Route path="/friends" element={<OurFriendsPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/add-pet" element={<AddPetPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 };
 
 export default App;
