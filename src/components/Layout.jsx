cca8de2b57ce4603967b8520bdf5fa47a26deab0
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-14 18:16:50 +0300

    feat: add Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,29 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Header from './Header/Header';
+import Loader from './Loader/Loader';
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <>
+      <Header />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            padding: '16px',
+            marginTop: '80px',
+          },
+        }}
+      />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default Layout;
