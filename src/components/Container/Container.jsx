72089e99c82cda4efacd6344781877eaa16e9704
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-16 23:19:10 +0300

    style: update Container styles

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,18 @@
+import clsx from 'clsx';
 import css from './Container.module.css';
 
-const Container = ({ children }) => {
-  return <div className={css.container}>{children}</div>;
+const Container = ({ children, padding = 'default' }) => {
+  return (
+    <div
+      className={clsx(
+        css.container,
+        padding === '32' && css['padding-32'],
+        padding === '64' && css['padding-64']
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
