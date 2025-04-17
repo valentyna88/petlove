0af917aebee80352a266d5f174e42699912a2cff
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-17 18:54:48 +0300

    feat: add Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,15 @@
-const Logo = () => {
-  return <div>Logo</div>;
+import { Link } from 'react-router-dom';
+import sprite from '../../assets/sprite.svg';
+import css from './Logo.module.css';
+
+const Logo = ({ icon }) => {
+  return (
+    <Link to="/home">
+      <svg className={css.logo} width={17} height={17}>
+        <use href={`${sprite}#${icon}`}></use>
+      </svg>
+    </Link>
+  );
 };
 
 export default Logo;
