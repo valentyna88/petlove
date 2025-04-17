eebcac9b47a8aac9eeb4080dfd1b9db1f705fa8f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-17 18:53:58 +0300

    feat: add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,24 @@
+import { useLocation } from 'react-router-dom';
+import Logo from '../Logo/Logo';
+import Nav from '../Nav/Nav';
+import clsx from 'clsx';
+import css from './Header.module.css';
+
 const Header = () => {
-  return <div>Header</div>;
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/home';
+
+  return (
+    <header
+      className={clsx(css.header, {
+        [css.homeHeader]: isHomePage,
+      })}
+    >
+      <Logo icon={isHomePage ? 'icon-logo-white' : 'icon-logo-black'} />
+      <Nav />
+    </header>
+  );
 };
 
 export default Header;
