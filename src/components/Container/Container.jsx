6f350b6e9037b4ae52b0878b239ee7c758e81acb
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-17 15:04:46 +0300

    style: add modifier-classes for the container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,13 +1,14 @@
 import clsx from 'clsx';
 import css from './Container.module.css';
 
-const Container = ({ children, padding = 'default' }) => {
+const Container = ({ children, padding = 'default', vertical = false }) => {
   return (
     <div
       className={clsx(
         css.container,
         padding === '32' && css['padding-32'],
-        padding === '64' && css['padding-64']
+        padding === '64' && css['padding-64'],
+        vertical && css['padding-vertical']
       )}
     >
       {children}
