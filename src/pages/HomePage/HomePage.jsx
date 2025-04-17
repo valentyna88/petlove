b4f3d332d654ec6510c4805aa5fbe755c5c758c6
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-17 15:05:16 +0300

    feat: add HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,21 @@
+import Container from '../../components/Container/Container';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container vertical>
+      <div className={css.titleBox}>
+        <h1 className={css.title}>
+          Take good <span className={css.span}>care</span> of your small pets
+        </h1>
+        <p className={css.text}>
+          Choosing a pet for your home is a choice that is meant to enrich your
+          life with immeasurable joy and tenderness.
+        </p>
+      </div>
+      <div className={css.bg}></div>
+    </Container>
+  );
 };
 
 export default HomePage;
