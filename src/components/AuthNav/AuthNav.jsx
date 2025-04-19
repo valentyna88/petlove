a5614ec232af01fea6efc7007c8b97fe69612caf
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-19 15:27:34 +0300

    feat: add AuthNav

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,5 +1,16 @@
+import css from './AuthNav.module.css';
+
 const AuthNav = () => {
-  return <div>AuthNav</div>;
+  return (
+    <div className={css.authNav}>
+      <button type="button" className={css.login}>
+        Log In
+      </button>
+      <button type="button" className={css.register}>
+        Registration
+      </button>
+    </div>
+  );
 };
 
 export default AuthNav;
