add178e1dc26bee4cbdc40f65307e8c2ecd7280b
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-19 21:34:21 +0300

    feat: add Nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import css from './Nav.module.css';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
+
+  return (
+    <nav className={css.nav}>
+      <NavLink to="/news" className={buildLinkClass}>
+        News
+      </NavLink>
+      <NavLink to="/notices" className={buildLinkClass}>
+        Find pet
+      </NavLink>
+      <NavLink to="/friends" className={buildLinkClass}>
+        Our friends
+      </NavLink>
+    </nav>
+  );
 };
 
 export default Nav;
