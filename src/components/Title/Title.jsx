f10eece65a2d58b5cdeca63af1ecfdc3742edbe0
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-20 15:18:36 +0300

    feat: add Title component

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,5 +1,11 @@
-const Title = () => {
-  return <div>Title</div>;
+import css from './Title.module.css';
+
+const Title = ({ children }) => {
+  return (
+    <>
+      <h1 className={css.title}>{children}</h1>
+    </>
+  );
 };
 
 export default Title;
