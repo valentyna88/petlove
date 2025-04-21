53f91ecad89f3450654107fc25332994336df690
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-21 23:24:29 +0300

    feat: add NewsItem component

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,5 +1,27 @@
-const NewsItem = () => {
-  return <div>NewsItem</div>;
+import { formatDate } from '../../utils/formatDate';
+import css from './NewsItem.module.css';
+
+const NewsItem = ({ item }) => {
+  const { imgUrl, title, text, date, url } = item;
+  return (
+    <>
+      <img src={imgUrl} alt={title} className={css.img} />
+      <h2 className={css.title}>{title}</h2>
+      <p className={css.text}>{text}</p>
+
+      <div className={css.readMoreWrapper}>
+        <span className={css.date}>{formatDate(date)}</span>
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={css.link}
+        >
+          Read more
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default NewsItem;
