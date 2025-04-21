3ab15543f8c5397b0b2ea5199bb209f8a781789f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-21 23:25:00 +0300

    feat: add NewsList

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,5 +1,27 @@
+import { useSelector } from 'react-redux';
+import { selectNews } from '../../redux/news/selectors';
+import NewsItem from '../NewsItem/NewsItem';
+import css from './NewsList.module.css';
+
 const NewsList = () => {
-  return <div>NewsList</div>;
+  const news = useSelector(selectNews);
+
+  return (
+    <>
+      {news.length === 0 && (
+        <p className={css.noResults}>
+          Sorry, no news found for this search parameter
+        </p>
+      )}
+      <ul className={css.newsList}>
+        {news.map(item => (
+          <li key={item.id} className={css.newsItem}>
+            <NewsItem item={item} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default NewsList;
