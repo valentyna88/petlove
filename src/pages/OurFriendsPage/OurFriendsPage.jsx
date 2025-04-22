065b5973b9095d15617940d95f0f038037c6b06c
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-22 21:55:24 +0300

    feat: add OurFriendsPage

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchFriends } from '../../redux/friends/operations';
+import Container from '../../components/Container/Container';
+import Title from '../../components/Title/Title';
+import FriendsList from '../../components/FriendsList/FriendsList';
+
 const OurFriendsPage = () => {
-  return <div>OurFriendsPage</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFriends());
+  }, [dispatch]);
+  return (
+    <Container padding="64">
+      <Title>Our friends</Title>
+      <FriendsList />
+    </Container>
+  );
 };
 
 export default OurFriendsPage;
