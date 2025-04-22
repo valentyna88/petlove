82fbc06541e2d9c5a15fb215481b10ec93c3c9fc
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-22 21:56:05 +0300

    feat: add FriendsList component

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,5 +1,22 @@
+import { useSelector } from 'react-redux';
+import { selectFriends } from '../../redux/friends/selectors';
+import FriendsItem from '../FriendsItem/FriendsItem';
+import css from './FriendsList.module.css';
+
 const FriendsList = () => {
-  return <div>FriendsList</div>;
+  const friends = useSelector(selectFriends);
+
+  return (
+    <>
+      <ul className={css.friendsList}>
+        {friends.map(friend => (
+          <li className={css.friendsItem} key={friend._id}>
+            <FriendsItem friend={friend} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default FriendsList;
