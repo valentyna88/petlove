3d2241c1eb4fa193bd9a5cb1d9f80ccbe7f60412
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-22 21:54:21 +0300

    feat: add friend selectors

---
+++ src/redux/friends/selectors.js
@@ -0,0 +1,3 @@
+export const selectFriends = state => state.friends.friends;
+export const selectIsLoading = state => state.news.isLoading;
+export const selectError = state => state.news.error;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import newsReducer from './news/slice';
+import friendsReducer from './friends/slice';
 
 export const store = configureStore({
   reducer: {
     news: newsReducer,
+    friends: friendsReducer,
   },
 });
