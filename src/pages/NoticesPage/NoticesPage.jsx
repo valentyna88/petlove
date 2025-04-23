55c3642bcd1566cc1b18fee68c72f012cc6e1545
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-23 23:02:17 +0300

    feat: add NoticesPage, connect notices state

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,5 +1,43 @@
+import { useDispatch, useSelector } from 'react-redux';
+import Container from '../../components/Container/Container';
+import NoticesFilters from '../../components/NoticesFilters/NoticesFilters';
+import NoticesList from '../../components/NoticesList/NoticesList';
+import Pagination from '../../components/Pagination/Pagination';
+import Title from '../../components/Title/Title';
+import {
+  selectCurrentPage,
+  selectTotalPages,
+} from '../../redux/notices/selectors';
+import { useEffect } from 'react';
+import { fetchNotices } from '../../redux/notices/operations';
+// import css from './NoticesPage.module.css';
+
 const NoticesPage = () => {
-  return <div>NoticesPage</div>;
+  const dispatch = useDispatch();
+  const currentPage = useSelector(selectCurrentPage);
+  const totalPages = useSelector(selectTotalPages);
+
+  useEffect(() => {
+    dispatch(fetchNotices({ page: 1, limit: 6 }));
+  }, [dispatch]);
+
+  const handlePageChange = page => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    dispatch(fetchNotices({ page, limit: 6 }));
+  };
+
+  return (
+    <Container>
+      <Title>Find your favorite pet</Title>
+      <NoticesFilters />
+      <NoticesList />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
+    </Container>
+  );
 };
 
 export default NoticesPage;
