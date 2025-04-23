72a6e6fa300774d1ca1877175ee2de20c87bb4ed
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-23 23:02:36 +0300

    feat: add NoticesList component

--- src/components/NoticesList/NoticesList.jsx
+++ src/components/NoticesList/NoticesList.jsx
@@ -1,5 +1,27 @@
+import { useSelector } from 'react-redux';
+import { selectNotices } from '../../redux/notices/selectors';
+import NoticesItem from '../NoticesItem/NoticesItem';
+import css from './NoticesList.module.css';
+
 const NoticesList = () => {
-  return <div>NoticesList</div>;
+  const notices = useSelector(selectNotices);
+
+  return (
+    <>
+      {notices.length === 0 && (
+        <p className={css.noResults}>
+          Sorry, no notices found for this search parameter
+        </p>
+      )}
+      <ul className={css.noticesList}>
+        {notices.map(notice => (
+          <li key={notice._id} className={css.noticesItem}>
+            <NoticesItem notice={notice} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default NoticesList;
