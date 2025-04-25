1786f86ebb50a9599c111cc092645e966fa036d0
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-25 22:09:15 +0300

    feat: add RegistrationPage and styles

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,5 +1,17 @@
+import Container from '../../components/Container/Container';
+import PetBlock from '../../components/PetBlock/PetBlock';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import css from './RegistrationPage.module.css';
+
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  return (
+    <Container>
+      <div className={css.registrationPage}>
+        <PetBlock />
+        <RegistrationForm />
+      </div>
+    </Container>
+  );
 };
 
 export default RegistrationPage;
