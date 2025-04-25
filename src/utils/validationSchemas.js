490cc1e77ae7ad714693fdf5424fc027bdd4b216
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-25 21:32:05 +0300

    feat: add validationSchemas to utils

---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,26 @@
+import * as yup from 'yup';
+
+// const numberRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+
+export const registerSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2, 'Name must be at least 2 characters')
+    .required('Name is required'),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .matches(
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      'Invalid email format'
+    )
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(7, 'Password must be at least 6 characters')
+    .required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm password is required'),
+});
