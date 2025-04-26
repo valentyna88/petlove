8ce8c26fba52b1ae8d2badf56919279a5ebfc5ab
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-26 14:49:35 +0300

    feat: add LoginPage and styles

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,17 @@
+import css from './LoginPage.module.css';
+import Container from '../../components/Container/Container';
+import LoginPetBlock from '../../components/LoginPetBlock/LoginPetBlock';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Container>
+      <div className={css.loginPage}>
+        <LoginPetBlock />
+        <LoginForm />
+      </div>
+    </Container>
+  );
 };
 
 export default LoginPage;
