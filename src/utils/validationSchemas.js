b3c00ab88e18f331e71ca01565cf3735c45df973
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-26 15:17:08 +0300

    feat: add loginSchema function to utils

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -17,10 +17,29 @@ export const registerSchema = yup.object().shape({
     .required('Email is required'),
   password: yup
     .string()
-    .min(7, 'Password must be at least 6 characters')
+    .min(7, 'Password must be at least 7 characters')
     .required('Password is required'),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref('password'), null], 'Passwords must match')
     .required('Confirm password is required'),
+});
+
+export const loginSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2, 'Name must be at least 2 characters')
+    .required('Name is required'),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .matches(
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      'Invalid email format'
+    )
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(7, 'Password must be at least 7 characters')
+    .required('Password is required'),
 });
