672a4dfe9a8aaf3a8df747e493b023fb49542038
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-07 17:24:14 +0300

    fix: fix header display and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import Container from '../Container/Container';
 import Logo from '../Logo/Logo';
 import Nav from '../Nav/Nav';
 import MobileMenu from '../MobileMenu/MobileMenu';
 import AuthNav from '../AuthNav/AuthNav';
+import UserNav from '../UserNav/UserNav';
 import clsx from 'clsx';
 import sprite from '../../assets/sprite.svg';
 import css from './Header.module.css';
@@ -12,8 +16,10 @@ import css from './Header.module.css';
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
-
   const isHomePage = location.pathname === '/home';
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -24,32 +30,44 @@ const Header = () => {
   };
 
   return (
-    <header
-      className={clsx(css.header, {
-        [css.homeHeader]: isHomePage,
-      })}
-    >
+    <header className={clsx(css.header, { [css.homeHeader]: isHomePage })}>
       <Container>
-        <div className={css.mobileHeader}>
-          <Logo icon={isHomePage ? 'icon-logo-white' : 'icon-logo-black'} />
-          {!isHomePage && <AuthNav className={css.authTablet} />}
-          <button type="button" className={css.hamburger} onClick={toggleMenu}>
-            <svg
-              className={clsx(
-                css.icon,
-                isHomePage ? css.whiteIcon : css.blackIcon
-              )}
-            >
-              <use href={`${sprite}#icon-burger`} />
-            </svg>
-          </button>
-        </div>
+        {!isDesktop && (
+          <div className={css.mobileHeader}>
+            <Logo icon={isHomePage ? 'icon-logo-white' : 'icon-logo-black'} />
 
-        <div className={css.desktopHeader}>
-          <Logo icon={isHomePage ? 'icon-logo-white' : 'icon-logo-black'} />
-          <Nav closeMenu={closeMenu} isHomePage={isHomePage} />
-          <AuthNav />
-        </div>
+            <div className={css.mobileNav}>
+              {isLoggedIn ? (
+                <UserNav />
+              ) : (
+                <AuthNav className={css.authTablet} />
+              )}
+
+              <button
+                type="button"
+                className={css.hamburger}
+                onClick={toggleMenu}
+              >
+                <svg
+                  className={clsx(
+                    css.icon,
+                    isHomePage ? css.whiteIcon : css.blackIcon
+                  )}
+                >
+                  <use href={`${sprite}#icon-burger`} />
+                </svg>
+              </button>
+            </div>
+          </div>
+        )}
+
+        {isDesktop && (
+          <div className={css.desktopHeader}>
+            <Logo icon={isHomePage ? 'icon-logo-white' : 'icon-logo-black'} />
+            <Nav closeMenu={closeMenu} isHomePage={isHomePage} />
+            {isLoggedIn ? <UserNav /> : <AuthNav />}
+          </div>
+        )}
 
         {menuOpen && (
           <MobileMenu closeMenu={closeMenu} isHomePage={isHomePage} />
