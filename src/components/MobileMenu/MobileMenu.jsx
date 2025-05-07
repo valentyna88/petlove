425f037a7fe59a58e44a733c84d994e6e592307f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-07 18:25:54 +0300

    fix: fix MobileMenu display and styles

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,26 +1,36 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import Nav from '../Nav/Nav';
 import AuthNav from '../AuthNav/AuthNav';
+import LogOutBtn from '../LogOutBtn/LogOutBtn';
 import sprite from '../../assets/sprite.svg';
-import css from './MobileMenu.module.css';
 import clsx from 'clsx';
+import css from './MobileMenu.module.css';
 
 const MobileMenu = ({ closeMenu, isHomePage }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <div className={clsx(css.menu, { [css.menuHome]: isHomePage })}>
+      <button
+        className={clsx(css.closeButton, { [css.closeButtonHome]: isHomePage })}
+        onClick={closeMenu}
+      >
+        <svg className={css.icon}>
+          <use href={`${sprite}#icon-cross-small`}></use>
+        </svg>
+      </button>
+
       <div className={css.menuContent}>
-        <button
-          className={clsx(css.closeButton, {
-            [css.closeButtonHome]: isHomePage,
-          })}
-          type="button"
-          onClick={closeMenu}
-        >
-          <svg className={css.icon}>
-            <use href={`${sprite}#icon-cross-small`}></use>
-          </svg>
-        </button>
         <Nav closeMenu={closeMenu} isHomePage={isHomePage} />
-        <AuthNav isHomePage={isHomePage} />
+
+        {isLoggedIn ? (
+          <div className={css.userNav}>
+            <LogOutBtn />
+          </div>
+        ) : (
+          <AuthNav />
+        )}
       </div>
     </div>
   );
