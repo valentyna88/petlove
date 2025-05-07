81492d0e7a30ae8369f9cb46e19290e8a7a30bd5
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-07 18:34:12 +0300

    feat: add UserBar component

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,5 +1,38 @@
-const UserBar = () => {
-  return <div>UserBar</div>;
+import { Link, useLocation } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import clsx from 'clsx';
+import css from './UserBar.module.css';
+import sprite from '../../assets/sprite.svg';
+
+const UserBar = ({ showName }) => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/home';
+
+  const user = useAuth();
+
+  return (
+    <>
+      <Link to="/profile" className={css.userLink}>
+        {user?.user?.avatar ? (
+          <img src={user.avatar} alt={`Avatar of ${user.user.name}`} />
+        ) : (
+          <svg width={20} height={20}>
+            <use href={`${sprite}#icon-user`} />
+          </svg>
+        )}
+      </Link>
+
+      {showName && (
+        <p
+          className={clsx(css.userName, {
+            [css.userNameHome]: isHomePage,
+          })}
+        >
+          {user.user.name}
+        </p>
+      )}
+    </>
+  );
 };
 
 export default UserBar;
