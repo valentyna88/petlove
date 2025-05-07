7d8704230f66548565658717291b9e1642c4f86c
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-07 18:28:52 +0300

    fix: add closeMenu function

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 import css from './AuthNav.module.css';
 import clsx from 'clsx';
 
-const AuthNav = ({ className = '' }) => {
+const AuthNav = ({ className = '', closeMenu }) => {
   return (
     <div className={clsx(css.authNav, className)}>
-      <Link to="/login" className={css.login}>
+      <Link to="/login" className={css.login} onClick={closeMenu}>
         Log In
       </Link>
-      <Link to="/register" className={css.register}>
+      <Link to="/register" className={css.register} onClick={closeMenu}>
         Registration
       </Link>
     </div>
