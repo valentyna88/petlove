3b2838f6344eabf4615f1ea8b6a4e825f9d6737d
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-07 18:32:11 +0300

    feat: update UserNav and styles

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,12 +1,20 @@
+import { useMediaQuery } from 'react-responsive';
 import UserBar from '../UserBar/UserBar';
 import LogOutBtn from '../LogOutBtn/LogOutBtn';
+import css from './UserNav.module.css';
+import clsx from 'clsx';
+import { useLocation } from 'react-router-dom';
 
 const UserNav = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/home';
+  const tablet = useMediaQuery({ minWidth: 768 });
+
   return (
-    <>
-      <UserBar />
-      <LogOutBtn />
-    </>
+    <div className={clsx(css.userNav, { [css.hideOnHome]: isHomePage })}>
+      {tablet && !isHomePage && <LogOutBtn />}
+      <UserBar showName={tablet} />
+    </div>
   );
 };
 
