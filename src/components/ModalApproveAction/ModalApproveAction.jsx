58c8935f59f310b067d608c3228651308e239827
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-22 20:54:33 +0300

    fix: fix a path to the cat icon

--- src/components/ModalApproveAction/ModalApproveAction.jsx
+++ src/components/ModalApproveAction/ModalApproveAction.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { logoutUser } from '../../redux/auth/operations';
 import ModalContainer from '../ModalContainer/ModalContainer';
+import catIcon from '../../assets/images/signin/cat-icon.png';
 import toast from 'react-hot-toast';
 import css from './ModalApproveAction.module.css';
 
@@ -24,7 +25,7 @@ const ModalApproveAction = ({ isOpen, onClose }) => {
   return (
     <ModalContainer isOpen={isOpen} onClose={onClose}>
       <div className={css.iconWrapper}>
-        <img src="/src/assets/images/signin/cat-icon.png" alt="Cat icon" />
+        <img src={catIcon} alt="Cat icon" />
       </div>
       <p className={css.text}>Already leaving?</p>
       <div className={css.actions}>
