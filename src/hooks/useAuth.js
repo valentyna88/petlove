aca164dd4d6cddf9e8e14be3843d3a2f87128090
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-24 01:17:35 +0300

    feat: add selectFavoritesNotices to useAuth hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,16 +3,19 @@ import {
   selectUser,
   selectIsLoggedIn,
   selectIsRefreshing,
+  selectFavoritesNotices,
 } from '../redux/auth/selectors';
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
+  const favoritesNotices = useSelector(selectFavoritesNotices);
 
   return {
     isLoggedIn,
     isRefreshing,
+    favoritesNotices,
     user,
   };
 };
