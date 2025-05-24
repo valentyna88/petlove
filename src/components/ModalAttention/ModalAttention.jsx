0fcfd9d3a318c218701f34a07e1b6adf08b82360
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-24 13:21:42 +0300

    style: add ModalAttention styles

--- src/components/ModalAttention/ModalAttention.jsx
+++ src/components/ModalAttention/ModalAttention.jsx
@@ -6,23 +6,25 @@ import css from './ModalAttention.module.css';
 const ModalAttention = ({ isOpen, onClose }) => {
   return (
     <ModalContainer isOpen={isOpen} onClose={onClose}>
-      <div className={css.iconWrapper}>
-        <img src={dogIcon} alt="Dog icon" />
-      </div>
-      <h3 className={css.title}>Attention</h3>
-      <p className={css.text}>
-        We would like to remind you that certain functionality is available only
-        to authorized users.If you have an account, please log in with your
-        credentials. If you do not already have an account, you must register to
-        access these features.
-      </p>
-      <div className={css.actions}>
-        <Link to="/login" className={css.login}>
-          Log In
-        </Link>
-        <Link to="/register" className={css.register}>
-          Registration
-        </Link>
+      <div className={css.modalContent}>
+        <div className={css.iconWrapper}>
+          <img src={dogIcon} alt="Dog icon" />
+        </div>
+        <h3 className={css.title}>Attention</h3>
+        <p className={css.text}>
+          We would like to remind you that certain functionality is available
+          only to authorized users.If you have an account, please log in with
+          your credentials. If you do not already have an account, you must
+          register to access these features.
+        </p>
+        <div className={css.actions}>
+          <Link to="/login" className={css.login}>
+            Log In
+          </Link>
+          <Link to="/register" className={css.register}>
+            Registration
+          </Link>
+        </div>
       </div>
     </ModalContainer>
   );
