ca21a1b533687778eec4f8ac89a09d41a40943b5
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-29 11:05:02 +0300

    feat: add UserBlock component

--- src/components/UserBlock/UserBlock.jsx
+++ src/components/UserBlock/UserBlock.jsx
@@ -1,5 +1,31 @@
-const UserBlock = () => {
-  return <div>UserBlock</div>;
+import { useAuth } from '../../hooks/useAuth';
+import sprite from '../../assets/sprite.svg';
+import css from './UserBlock.module.css';
+
+const UserBlock = ({ onOpenModalEditUser }) => {
+  const { user } = useAuth();
+  return (
+    <div className={css.userAvatarBox}>
+      <div className={css.userAvatarWrapper}>
+        {user?.avatar ? (
+          <img src={user.avatar} alt={`Avatar of ${user.name}`} />
+        ) : (
+          <svg width={40} height={40}>
+            <use href={`${sprite}#icon-user`}></use>
+          </svg>
+        )}
+      </div>
+      {!user.avatar && (
+        <button
+          className={css.userAvatarBtn}
+          type="button"
+          onClick={onOpenModalEditUser}
+        >
+          Upload photo
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default UserBlock;
