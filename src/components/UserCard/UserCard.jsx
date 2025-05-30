51b9de2b09212a2d47df1a4679e281092801affa
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-30 13:29:04 +0300

    feat: add UserCard component

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,5 +1,18 @@
+import EditUserBtn from '../EditUserBtn/EditUserBtn';
+import LogOutBtn from '../LogOutBtn/LogOutBtn';
+import PetsBlock from '../PetsBlock/PetsBlock';
+import UserBlock from '../UserBlock/UserBlock';
+import css from './UserCard.module.css';
+
 const UserCard = () => {
-  return <div>UserCard</div>;
+  return (
+    <section className={css.userCard}>
+      <EditUserBtn />
+      <UserBlock />
+      <PetsBlock />
+      <LogOutBtn />
+    </section>
+  );
 };
 
 export default UserCard;
