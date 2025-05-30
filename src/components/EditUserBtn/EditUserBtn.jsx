6619d4dda5a4894e023debc39c8e6f6d33215346
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-30 16:35:06 +0300

    feat: add EditUserBtn component

--- src/components/EditUserBtn/EditUserBtn.jsx
+++ src/components/EditUserBtn/EditUserBtn.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import ModalEditUser from '../ModalEditUser/ModalEditUser';
+import sprite from '../../assets/sprite.svg';
+import css from './EditUserBtn.module.css';
+
 const EditUserBtn = () => {
-  return <div>EditUserBtn</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  return (
+    <>
+      <div className={css.userBoxWrapper}>
+        <div className={css.userBox}>
+          <p>User</p>
+          <svg width={18} height={18}>
+            <use href={`${sprite}#icon-user`} />
+          </svg>
+        </div>
+
+        <button type="button" className={css.editBtn} onClick={openModal}>
+          <svg className={css.icon} width={18} height={18}>
+            <use xlinkHref={`${sprite}#icon-edit`} />
+          </svg>
+        </button>
+      </div>
+      <ModalEditUser isOpen={isModalOpen} onClose={closeModal} />
+    </>
+  );
 };
 
 export default EditUserBtn;
