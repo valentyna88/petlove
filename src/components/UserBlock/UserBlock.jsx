0bbec96a29bc971c55b3ef4809e298bc8c01cfd8
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-30 16:36:55 +0300

    feat: add UserBlock component

--- src/components/UserBlock/UserBlock.jsx
+++ src/components/UserBlock/UserBlock.jsx
@@ -2,29 +2,42 @@ import { useAuth } from '../../hooks/useAuth';
 import sprite from '../../assets/sprite.svg';
 import css from './UserBlock.module.css';
 
-const UserBlock = ({ onOpenModalEditUser }) => {
+const UserBlock = () => {
   const { user } = useAuth();
   return (
-    <div className={css.userAvatarBox}>
-      <div className={css.userAvatarWrapper}>
-        {user?.avatar ? (
-          <img src={user.avatar} alt={`Avatar of ${user.name}`} />
-        ) : (
-          <svg width={40} height={40}>
-            <use href={`${sprite}#icon-user`}></use>
-          </svg>
+    <>
+      <div className={css.userAvatarBox}>
+        <div className={css.userAvatarWrapper}>
+          {user?.avatar ? (
+            <img src={user.avatar} alt={`Avatar of ${user.name}`} />
+          ) : (
+            <svg width={40} height={40}>
+              <use href={`${sprite}#icon-user`}></use>
+            </svg>
+          )}
+        </div>
+        {!user.avatar && (
+          <button className={css.uploadPhotoBtn} type="button">
+            Upload photo
+          </button>
         )}
       </div>
-      {!user.avatar && (
-        <button
-          className={css.userAvatarBtn}
-          type="button"
-          onClick={onOpenModalEditUser}
-        >
-          Upload photo
-        </button>
-      )}
-    </div>
+
+      <div className={css.myInfoWrapper}>
+        <h2 className={css.myInfoTitle}>My information</h2>
+        <ul className={css.myInfoList}>
+          <li style={{ borderColor: user.name && 'var(--color-accent)' }}>
+            {user?.name}
+          </li>
+          <li style={{ borderColor: user.email && 'var(--color-accent)' }}>
+            {user?.email}
+          </li>
+          <li style={{ borderColor: user.phone && 'var(--color-accent)' }}>
+            {user?.phone || '+380'}
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
