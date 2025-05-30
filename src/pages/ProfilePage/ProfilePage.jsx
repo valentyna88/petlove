4fc6f158b0a0277c0490ee9a9147526c03355c81
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-05-30 13:27:36 +0300

    feat: add ProfilePage

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,14 @@
+import Container from '../../components/Container/Container';
+import UserCard from '../../components/UserCard/UserCard';
+import MyNotices from '../../components/MyNotices/MyNotices';
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <Container>
+      <UserCard />
+      <MyNotices />
+    </Container>
+  );
 };
 
 export default ProfilePage;
